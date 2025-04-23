83c0f0485828cb80116e8554dbcf8a3974b219c8
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-23 07:30:15 +0530

    password

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -14,10 +14,10 @@ const UserSchema=new mongoose.Schema({
         lowecase:true
     },
 
-    // password:{
-    //     type: String,
-    //     required:true,
-    // }
+    password:{
+        type: String,
+        required:true,
+    }
 
 })
 
