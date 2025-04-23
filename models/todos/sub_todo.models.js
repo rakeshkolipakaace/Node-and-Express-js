f585007b7e0086672be5219bcf8ccae833d236bc
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-23 07:29:27 +0530

    username

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,24 @@
 import mongoose from 'mongoose';
 
-const UserSchema=new mongoose.Schema({})
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true
+    },
+    // email:{
+    //     type:String,
+    //     required:true,
+    //     unique:true,
+    //     lowecase:true
+    // },
+
+    // password:{
+    //     type: String,
+    //     required:true,
+    // }
+
+})
 
 export const User=mongoose.model("User",UserSchema)
