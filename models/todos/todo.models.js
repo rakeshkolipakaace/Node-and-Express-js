fbc36e3a9f62890b5e80bbf89cc9977246ce03e8
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-28 22:34:22 +0530

    todo.models.js

--- models/todos/todo.models.js
+++ models/todos/todo.models.js
@@ -2,6 +2,31 @@ import mongoose from 'mongoose';
 
 const todoSchema=new mongoose.Schema({
 
+    content:{
+        type: String,
+        required: true,
+
+
+    },
+
+    complete:{
+        type: Boolean,
+        default :false
+    },
+
+    createdBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+
+    subTodos:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Subtodoes"
+    }]
+
+
+
 
 
 
