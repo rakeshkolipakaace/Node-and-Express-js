c2133832fcebb3d8b41fa87bb73203250e0c1aab
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-28 21:58:55 +0530

    subtodo model

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,24 +1,6 @@
 import mongoose from 'mongoose';
 
-const UserSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowecase:true
-    },
+const subtodoSchema=new mongoose.Schema({},{timestamps:true})
 
-    password:{
-        type: String,
-        required:[true, "password is required"]
-    }
 
-},{timestamps:true})
-
-export const User=mongoose.model("User",UserSchema)
+export const Subtodos=mongoose.model("Subtodoes",SubtodoSchema)
