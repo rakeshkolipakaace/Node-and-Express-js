61b0e1ceb7684f55ccd7aae86887fc986eba9726
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-28 22:24:58 +0530

    todo.modules.js

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
-const subtodoSchema=new mongoose.Schema({},{timestamps:true})
+const subtodoSchema=new mongoose.Schema({
+
+    
+
+
+},{timestamps:true})
 
 
 export const Subtodos=mongoose.model("Subtodoes",SubtodoSchema)
