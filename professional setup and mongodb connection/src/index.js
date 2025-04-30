a12f5f059738080294546d2cb2358cd5cf278e92
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-30 07:17:17 +0530

    db connection approach 1

---
+++ professional setup and mongodb connection/src/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "./constants";
+
+import express from "express";
+
+const app=express()
+
+(async()=>{
+try{
+   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    application.on("error",(error)=>{
+        console.log("ERRR: ",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is Listening on port ${process.env.PORT}`)
+    })
+} catch(error){
+    console.error("ERROR: ",error)
+    throw err
+}
+})()
