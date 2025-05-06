cb5e7e7a94de8ef49c2e2665c3d6ba7615b3ce84
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 19:34:46 +0530

    ApiError.js

---
+++ professional setup and mongodb connection/src/utils/ApiError.js
@@ -0,0 +1,29 @@
+class ApiError extends Error{
+    constructor(
+        statuscode,
+        something="Something went wrong",
+        errors=[],
+        stack=""
+    ){
+        super(something)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false;
+        this.errors=errors
+
+
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor) 
+        }
+
+
+
+    }
+}
+
+
+export {ApiError}
