b5131daa04f0e0054a10d3c594ed0a1643bb6511
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 19:03:05 +0530

    app.js

--- professional setup and mongodb connection/src/app.js
+++ professional setup and mongodb connection/src/app.js
@@ -14,5 +14,10 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 export {app}
