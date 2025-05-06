7b9c32712dd6c5074a0fab3b3b3bc733007cb027
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 19:58:43 +0530

    user.model.js

--- professional setup and mongodb connection/src/models/user.model.js
+++ professional setup and mongodb connection/src/models/user.model.js
@@ -1,5 +1,63 @@
 import mongoose from 'mongoose';
 
-const  userSchema=new Schema({})
+const  userSchema=new Schema({
+
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+
+    },
+
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+
+
+    password:{
+        type: Number,
+        required:true,
+        trim:true,
+        index:true
+    },
+
+    avatar:{
+        type: String, //cloudinary url
+        required:true,
+
+
+
+    },
+    coverImage:{
+        type: String
+    },
+
+    watchHistory:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+
+    }],
+
+    password:{
+        type: String,
+        required:[true,"Password is required"],
+       
+    },
+
+    refreshToken:{
+        type: String
+    }
+
+    
+},{
+    timestamps:true,
+})
 
 export const User=mongoose.model("User",userSchema)
