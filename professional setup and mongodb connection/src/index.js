5667c195b96249eb77984999b255b0c47a0cf170
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 18:43:45 +0530

    new changes

--- professional setup and mongodb connection/src/index.js
+++ professional setup and mongodb connection/src/index.js
@@ -12,6 +12,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed !!!",err);
+})
 
 
 
