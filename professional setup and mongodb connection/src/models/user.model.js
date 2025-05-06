23a581926887b27cb1c668a2551a6fabfdf4481f
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 22:28:19 +0530

    user.model.js

--- professional setup and mongodb connection/src/models/user.model.js
+++ professional setup and mongodb connection/src/models/user.model.js
@@ -36,6 +36,13 @@ const  userSchema=new Schema({
         index:true
     },
 
+    fullname:{
+        type: String,
+        required:true,
+        trim:true,
+        index:true
+    },
+
     avatar:{
         type: String, //cloudinary url
         required:true,
@@ -78,6 +85,28 @@ userSchema.pre("save",async function(next){
 
 userSchema.methods.comparePassword=async function(password){
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username:this.username,
+        fullname: this.fullname
+    },
+process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+})
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id: this._id,
+        
+    },
+process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+})
 }
 
 
