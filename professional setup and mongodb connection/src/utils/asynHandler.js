907e6b51938e8eb210c15602fee3c8dd1a83bd8c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-06 19:19:46 +0530

    asyHandler1

--- professional setup and mongodb connection/src/utils/asynHandler.js
+++ professional setup and mongodb connection/src/utils/asynHandler.js
@@ -8,14 +8,24 @@ export {asyncHandler}
 
 
 
-const asyncHandler=(fn)=>async()=>{
-    try{
-        await fn(req,res,next)
-    }
-    catch(error){
-        res.status(error.code||500).json({
-            success:false,
-            message:error.message
-        })
-    }
-}
+
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(fn)=>()=>{}
+// const asyncHandler=(fn)=>async()=>{}
+
+
+
+
+
+// const asyncHandler=(fn)=>async()=>{
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch(error){
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
